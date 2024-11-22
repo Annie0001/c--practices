139b42615ff724c3fa9206b03e11e658ee781e9a
Annie <aniohanian@gmail.com>
2024-11-21 18:24:44 -0800

    Conditional statements

---
+++ The_if_Decision_Statement/ConsoleApp13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Variables
+{
+    class ConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ?"boat":"strand of lint";
+            Console.Write("You won a");
+            Console.Write(message);
+            Console.Write(".");
+            Console.ReadLine();
+        }
+    }
+}
