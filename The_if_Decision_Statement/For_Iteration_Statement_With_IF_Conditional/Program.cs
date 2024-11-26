6c8ae2a99dbed894dcb9125c40e6d60c276e2ebc
Annie <aniohanian@gmail.com>
2024-11-25 23:53:48 -0800

    Conditional Statement Examples

---
+++ The_if_Decision_Statement/For_Iteration_Statement_With_IF_Conditional/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace For_Iteration_With_If_conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<10; i++)
+            {   
+                // Console.WriteLine(i);
+                if (i==7)
+                {
+                    Console.WriteLine("Found Seven!");
+                    break;
+                }
+            }
+        }
+    }
+}
