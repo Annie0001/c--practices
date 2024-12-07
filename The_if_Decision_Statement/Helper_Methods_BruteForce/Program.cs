9b30969d14f8ebcae32a3dcc3df51187e9fbecdf
Annie <aniohanian@gmail.com>
2024-12-07 11:37:35 -0800

    Helper Methods Brute Force

---
+++ The_if_Decision_Statement/Helper_Methods_BruteForce/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Net.NetworkInformation;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+            
+            Console.Write("What's your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your Last name?");
+            string lastName = Console.ReadLine();
+
+            Console.Write("In What city were you born?");
+            string city = Console.ReadLine();
+
+            char[] firstNameArray = firstName.ToCharArray();
+            Array.Reverse(firstNameArray);
+            
+            char[] LastNameArray = lastName.ToCharArray();
+            Array.Reverse(LastNameArray);
+
+            char[] cityArray = city.ToCharArray();
+            Array.Reverse(cityArray);
+
+            string result ="";
+            foreach(char item in firstNameArray)
+            {
+                result += item;
+            }
+            result +=" ";
+            foreach(char item in LastNameArray)
+            {
+                result += item;
+            }
+            result +=" ";
+            foreach(char item in cityArray)
+            {
+                result += item;
+            }
+            Console.WriteLine("Results:"+result);
+            Console.ReadLine();
+        }
+    }
+}
