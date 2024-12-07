11d2e3f59da9347dcb655f4880fb6d4f27a0f3ee
Annie <aniohanian@gmail.com>
2024-12-07 13:08:37 -0800

    Calling Functions

---
+++ The_if_Decision_Statement/Helper_Methods_Reversing_String/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game!");
+            ReverseString();
+            Console.ReadLine();
+        }
+        private static void ReverseString()
+        {
+            string message="Hello World!";
+            char [] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
