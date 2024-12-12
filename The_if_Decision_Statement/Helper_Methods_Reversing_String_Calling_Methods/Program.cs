53689cde5229f9f40c1281b23cab3b13184694aa
Annie <aniohanian@gmail.com>
2024-12-11 23:57:46 -0800

    caaling function inside static void main to reverse a string

---
+++ The_if_Decision_Statement/Helper_Methods_Reversing_String_Calling_Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game!");
+            ReverseString();
+            Console.ReadLine();
+        }
+        private static void ReverseString(string message)
+        {
+            // string message="";
+            char [] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" ");
+        }
+    }
+}
