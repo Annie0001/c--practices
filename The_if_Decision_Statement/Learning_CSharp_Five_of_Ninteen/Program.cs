1d47b9f1e9df69181aac39a66f5396fb057591b2
Annie <aniohanian@gmail.com>
2024-12-22 11:28:34 -0800

    strings exercise

---
+++ The_if_Decision_Statement/Learning_CSharp_Five_of_Ninteen/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string firstFriend = "Maria";
+string secondFriend = "Scott";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
