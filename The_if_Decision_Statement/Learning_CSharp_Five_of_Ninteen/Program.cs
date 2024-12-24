81636f13f6817aa2a618d881e7b6567dd2860d53
Annie <aniohanian@gmail.com>
2024-12-24 00:00:19 -0800

    strings

--- The_if_Decision_Statement/Learning_CSharp_Five_of_Ninteen/Program.cs
+++ The_if_Decision_Statement/Learning_CSharp_Five_of_Ninteen/Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string firstFriend = "Maria";
-string secondFriend = "Scott";
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+// Console.WriteLine("Hello, World!");
+// string firstFriend = "Maria";
+// string secondFriend = "Scott";
+// Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+string greeting = " Hello World! ";
+Console.WriteLine($"[{greeting}]");
+Console.WriteLine("-------------------");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+Console.WriteLine("-------------------");
+
+trimmedGreeting=greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+Console.WriteLine("----------------------");
+
+trimmedGreeting=greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
