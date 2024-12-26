1b35ff338a7c225b82d5e7b35dd9c67f5dcb95cc
Annie <aniohanian@gmail.com>
2024-12-25 23:51:30 -0800

    String functions

---
+++ The_if_Decision_Statement/Learning_CSharp_Six_of_Ninteen/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+string firstFriend="Maria";
+string secondFriend="Scott";
+string Friends=$"My friends are {firstFriend} and {secondFriend}";
+Console.WriteLine(Friends.Contains("Scott"));
+Console.WriteLine(Friends.ToUpper());
+Console.WriteLine(Friends.ToLower());
+Console.WriteLine(Friends.Length);
