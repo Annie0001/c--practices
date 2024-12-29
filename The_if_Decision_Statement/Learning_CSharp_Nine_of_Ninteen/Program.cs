d94d89432b3fb5c113c6237c564842964817c38c
Annie <aniohanian@gmail.com>
2024-12-28 22:36:46 -0800

    Conditionals

--- The_if_Decision_Statement/Learning_CSharp_Nine_of_Ninteen/Program.cs
+++ The_if_Decision_Statement/Learning_CSharp_Nine_of_Ninteen/Program.cs
@@ -53,3 +53,55 @@ while (counter < 10)
     Console.WriteLine($"Hello World! The counter is {counter}");
     counter++;
 }
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("                                           ");
+
+int counter1=0;
+do{
+    Console.WriteLine($"Hello World! The counter is {counter1}");
+    counter1++;
+}
+while (counter1 < 10);
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("                                           ");
+
+for (int counter2=0; counter2 < 10; counter2++)
+{
+    Console.WriteLine($"Hello World! The counter is {counter2}");
+}
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("                                           ");
+
+for (int row=1 ; row< 11; row++)
+{
+    for (char column='a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("                                           ");
+
+/*find the sum of all integers 1 through 20 that are divisible by 3. Here are a few hints:
+The % operator gives you the remainder of a division operation.
+The if statement gives you the condition to see if a number should be part of the sum.
+The for loop can help you repeat a series of steps for all the numbers 1 through 20.
+Try it yourself. Then check how you did. As a hint, you should get 63 for an answer.*/
+
+int sum=0;
+for (int number=1; number< 20; number ++)
+{
+    if (number % 3 == 0)
+    {
+        sum = sum + number;
+    }
+}
+Console.WriteLine($"Sum is: {sum}");
