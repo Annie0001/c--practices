9b8d26878556c049f430b079fd3c7a2fb9605588
Annie <aniohanian@gmail.com>
2024-12-29 21:49:53 -0800

    while Loop

---
+++ The_if_Decision_Statement/Learning_CSharp_Eleven_of_Ninteen/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int counter=0;
+while (counter<10)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
