e315a72708d9d4d479a3b4297de1306823fb10ce
Annie <aniohanian@gmail.com>
2025-01-01 23:58:31 -0800

    Part 11, 12, 13, 14 of 19

---
+++ The_if_Decision_Statement/Learning_CSharp_Thirteen_For_Loop_Range_of_Ninteen/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+var names = new List <string> {"Ann","Jeff","Bob","Kiki"};
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("------------------------------");
+Console.WriteLine(names[names.Count-1]);
+
+Console.WriteLine("------------------------------");
+Console.WriteLine("------------------------------");
+Console.WriteLine(names[^1]);
+
+Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+foreach (var name in names[1..3])
+{
+    Console.WriteLine(name);
+}
