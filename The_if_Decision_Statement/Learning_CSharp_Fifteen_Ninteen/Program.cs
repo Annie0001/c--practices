e76e84728f0a5c9ffddde85e0684856c66ff5565
Annie <aniohanian@gmail.com>
2025-01-02 20:30:56 -0800

    LINQ syntax

---
+++ The_if_Decision_Statement/Learning_CSharp_Fifteen_Ninteen/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//var score = new List <int> [97,92,81,60];
+int[] scores =[97,92,81,60];
+
+IEnumerable<int> scoreQuery=
+        from score in scores
+        where score > 80
+        select score;
+
+foreach (var i in scoreQuery)
+{
+    Console.WriteLine(i+" ");
+}
