c51964831c06e2bafe0feec8a18bcccd58d59c9c
Annie <aniohanian@gmail.com>
2025-01-03 15:26:50 -0800

    LINQ Query Expressions

---
+++ The_if_Decision_Statement/Learning_CSharp_Seventeen_of_Ninteen/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+var scores = new List<int>{3,45,82,97,92,100,81,60};
+
+// IEnumerable <string> scoreQuery=
+//             from score in scores
+//             where score>80
+//             orderby score descending
+//             select $"The score is {score}";
+
+// foreach (string s in scoreQuery)
+// {
+//     Console.WriteLine(s);
+// }
+
+Console.WriteLine("________________________________");
+Console.WriteLine("________________________________");
+
+IEnumerable <int> scoreQuery=
+            from score in scores
+            where score>80
+            orderby score descending
+            select score;
+
+// To make the list of the scores questioned/queried
+// when we changed from string back to int and llist them
+// now we have literal list of ints 
+List <int> myscores = scoreQuery.ToList();
+
+foreach (var score in myscores)
+{
+    Console.WriteLine(score);
+}
+
+
+Console.WriteLine("________________________________");
+Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+// IEnumerable <string> scoreQuery=
+//             from score in scores
+//             where score>80
+//             orderby score descending
+//             select $"The score is {score}";
+//                                Line 7
+var scoreQuery1 = scores.Where(s =>s > 80).OrderByDescending(s => s);
+
+List <int> myscores1 = scoreQuery1.ToList();
+
+foreach (var score in myscores1)
+{
+    Console.WriteLine(score);
+}
---
+++ The_if_Decision_Statement/Learning_CSharp_Sixteen_of_Ninteen/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+var scores = new List <int> {97,92,81,60};
+// IEnumerable <int> ScoreQuery=
+//             from score in scores
+//             where score>80
+//             orderby score descending
+//             select score;
+
+// foreach (int i in ScoreQuery)
+// {
+//     Console.Write(i+ " ");
+// }
+
+IEnumerable <string> ScoreQuery=
+            from score in scores
+            where score>80
+            orderby score descending
+            select $"The score is {score}";
+
+foreach (string s in ScoreQuery)
+{
+    Console.WriteLine(s);
+}
