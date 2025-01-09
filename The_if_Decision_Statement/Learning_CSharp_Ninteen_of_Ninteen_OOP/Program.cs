2eda3e88f591df9abe6c583a8e7c9860fe4d3f8b
Annie <aniohanian@gmail.com>
2025-01-09 03:10:06 -0800

    OOP Exercise

---
+++ The_if_Decision_Statement/Learning_CSharp_Ninteen_of_Ninteen_OOP/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using Microsoft.VisualBasic;
+
+var p1= new Person("Scott","Smith",new DateOnly(1970,1,1));
+var p2= new Person("Aaron","Farfala",new DateOnly(1982,1,1));
+
+var PersonList = new List <Person>{p1,p2};
+foreach( var person in PersonList)
+{
+    Console.WriteLine(person.getFirstName());
+}
+public class Person
+{
+    private string firstname;
+    private string lastname;
+    private DateOnly birthday;
+
+    public Person(string fName,string lName,DateOnly bd)
+    {
+        firstname = fName;
+        lastname = lName;
+        birthday = bd;
+
+    }
+    public string getFirstName()
+    {
+        return firstname;
+    }
+
+}
+
