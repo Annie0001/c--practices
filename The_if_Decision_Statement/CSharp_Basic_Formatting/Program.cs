1a1ac79ae360caac8353c11cbf915755711b4bd6
Annie <aniohanian@gmail.com>
2025-01-17 22:28:46 -0800

    CSharp String Exercise

---
+++ The_if_Decision_Statement/CSharp_Basic_Formatting/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Add Line after 
+Console.WriteLine("Hello\nWorld!");
+
+// Add Tab between text
+Console.WriteLine("Hello\tWorld!");
+
+//Add Quatation Mark
+Console.WriteLine("Hello \"World\"!");
+
+//Add BAckSlash
+Console.WriteLine("Hello c:\\hello\\World");
+Console.WriteLine("\n");
+
+//Example
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1021\t\tComplete!\n");
+Console.WriteLine("Outpu Directory:\t");
+
+// Verbatim string literal --- Meaning exactly word by word
+// A verbatim string literal will keep all whitespace 
+// and characters without the need to escape the backslash.
+Console.WriteLine(@"     c:\source\repos
+        (this is where your code goes)");
+
+// Output: C:\Output\First-Project\Data
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+// Exercise
+string projectNamee = "ACME";
+string passage = $@"c:\Exercise\{projectNamee}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{passage}");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string passage1=$@"c:\Exercise\{projectNamee}\ru-RU\data.txt";
+Console.WriteLine($"russianMessage:\n\t\t{passage1}");
--- The_if_Decision_Statement/Learning_CSharp_Ninteen_of_Ninteen_OOP/Program.cs
+++ The_if_Decision_Statement/Learning_CSharp_Ninteen_of_Ninteen_OOP/Program.cs
@@ -7,10 +7,22 @@ using Microsoft.VisualBasic;
 var p1= new Person("Scott","Smith",new DateOnly(1970,1,1));
 var p2= new Person("Aaron","Farfala",new DateOnly(1982,1,1));
 
+// Add dogs for Scott
+p1.GetPets().Add(new Dog("JJ","Grey"));
+p1.GetPets().Add(new Dog("XoXo","White"));
+// Add cats for Aaron
+p2.GetPets().Add(new Cat("Lolo","Brown"));
+p2.GetPets().Add(new Cat("Phiphi","Blue"));
+
 var PersonList = new List <Person>{p1,p2};
 foreach( var person in PersonList)
 {
-    Console.WriteLine(person.getFirstName());
+    // Console.WriteLine(person.gGetFirstName());
+    Console.WriteLine(person);
+    foreach ( var pet in person.GetPets())
+    {
+        Console.WriteLine($" pet: {pet} ");
+    }
 }
 public class Person
 {
@@ -18,17 +30,69 @@ public class Person
     private string lastname;
     private DateOnly birthday;
 
+    private List<Pet> pets = new List<Pet>{};
+
     public Person(string fName,string lName,DateOnly bd)
     {
         firstname = fName;
         lastname = lName;
-        birthday = bd;
+        birthday = bd;                 
 
     }
-    public string getFirstName()
+    public List<Pet> GetPets()
+    {
+        return pets;
+    }
+    public string GetFirstName()
     {
         return firstname;
     }
 
+    // Override method used ti stringify the person object's properties first name and last name
+    public override string ToString()
+    {
+        // You can return any string that you want such as "good person" so 
+        // when you console.write(person), it will print out good person.
+        return $"{firstname} {lastname}";
+    }
+}
+public abstract class Pet
+{
+    //private string name;
+    // we changed this scope to protected so that the classes that derive this abstract class can inherite
+    // this property. In this case we used this field for both Dog and Cat classes
+    protected string name;
+    //private string color;
+    protected string color;
+    public abstract string Makesound();
+
+    public override string ToString()
+    {
+        return $"{name} {color}";
+    }
+}
+public class Dog : Pet
+{
+    public Dog(string dogName, string dogColor)
+    {
+        name = dogName;
+        color = dogColor;
+    }
+    public override string Makesound()
+    {
+        return "Bark";
+    }
+}
+public class Cat : Pet
+{   
+    public Cat(string catName, string catColor)
+    {
+        name = catName;
+        color = catColor;
+    }
+    public override string Makesound()
+    {
+        return "Meow";
+    }
 }
 
